ab3abc97e833528204a153bb824771a0fdf764b8
Carlo Palladino”git config --global user.name CarlosPalladino” <carlosezequiel19palladino@gmail.com>
2023-03-08 20:45:50 -0300

    delete cambiado + conexión a postgres

--- crudsito/settings.py
+++ crudsito/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'crudsito.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tareasdb',
+        'USER': 'postgres',
+        'PASSWORD': 'carlos',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -118,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-LOGIN_URL = '/signin' 
+LOGIN_URL = '/signin'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
